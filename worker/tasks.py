45d0747a311ebef3c0d160dcb201b322ce3c3176
Wilco Schoneveld <schoneveld.wj@gmail.com>
2020-05-07 11:55:03 +0200

    Added postrun handler

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,7 +1,16 @@
 from celery import Celery
+from celery.signals import task_postrun
+
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@rabbit//')
 
 @app.task
 def add(x, y):
+    import time
+    time.sleep(5)
     return x + y
+
+
+@task_postrun.connect
+def task_postrun_handler(**kwargs):
+    print("postrun handler", kwargs)
